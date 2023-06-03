291d0476309b871d300b65f4116b727cfab61608
AnnPak <pandaqwerty1357@yandex.ru>
2023-06-03 17:37:13 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const token = require('./config.js');
+
+const TelegramBot = require('node-telegram-bot-api');
+
+const bot = new TelegramBot(token, {polling: true});
+
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+
+  // send a message to the chat acknowledging receipt of their message
+  bot.sendMessage(chatId, 'Received your message');
+});
